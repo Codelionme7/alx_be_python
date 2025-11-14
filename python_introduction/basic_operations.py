d2ece53ca19a0547eaba6dba934eb8479e1cffda
Codelionme7 <mugishalionel02@gmail.com>
2025-11-14 23:56:01 +0300

    Fix basic_operations.py: exact ALX variable assignment format

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,13 @@
-# Define variables
-number1=10
-number2=5
+# Assign values to variables
+number1 = 10
+number2 = 5
 
 # Perform arithmetic operations
-addition=number1+number2
-subtraction=number1-number2
-multiplication=number1*number2
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
 
-# Print the results
-print("Addition of",number1,"and",number2,"is",addition)
-print("Subtraction of",number1,"and",number2,"is",subtraction)
-print("Multiplication of",number1,"and",number2,"is",multiplication)
+# Print results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
